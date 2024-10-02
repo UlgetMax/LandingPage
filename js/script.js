6a9267a7c7e18c38883f41289add56c3bef5dcd6
Max <trevarcat@mail.ru>
2024-10-02 17:15:27 +0300

    L-12: Hide the block of additional services when clicking on the header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-let currentIndex = 0;
-const totalSlides = document.querySelectorAll('input[name="slider-reviews"]').length;
-
-setInterval(() => {
-    currentIndex = (currentIndex + 1) % totalSlides;
-    document.getElementById(`slider${currentIndex + 1}`).checked = true;
-}, 5000);
+document.querySelector('.feedback__additonService-title').addEventListener('click', function() {
+    this.classList.toggle('active');
+    
+    const serviceBlock = document.querySelector('.additonService__block');
+    serviceBlock.classList.toggle('hidden');
+});
